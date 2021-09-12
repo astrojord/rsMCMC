90bae04b0f4957b1c4f0e976496cc9fa92f2c49d
Jordan <astrojord@gmail.com>
2021-09-12 16:33:01 -0400

    framework of final main

---
+++ main.py
@@ -0,0 +1,21 @@
+import numpy as np
+import sys
+from mcStep import *
+from Ability import * 
+
+# EXPECTED COMMAND LINE USAGE:
+# $ python main.py [args in an order that i will determine later]
+
+def main(args):
+    # woooo
+    # unpack args
+    # instantiate player and target 
+    # instantiate abilities
+    # start with n length sequence of all wrack/piercing shot/slice
+    # run MCMC for specified iterations
+    # store damage of each iteration
+    # plot damage over each iteration and return final sequence 
+
+if __name__ == '__main__':
+    args = sys.argv
+    main(args)
--- mcStep.py
+++ mcStep.py
@@ -6,18 +6,18 @@ from Ability import Ability
 from SelfParam import SelfParam
 from TargetParam import TargetParam
 
-def mcStep(n, current, player, target, hitChance, abilityDmg):
+def mcStep(n, current, player, target, abilities, hitChance, abilityDmg):
     # calculate the damage of the current sequence
     currentDmg = calcSequenceDamage(current, player, target, hitChance, abilityDmg)
 
     # generate a new sequence of abilities of length n and get its damage
-    new = generateSequence(n, player, target)
+    new = generateSequence(n, abilities, player, target)
     newDmg = calcSequenceDamage(new, player, target, hitChance, abilityDmg)
 
     # determine whether to keep or reject new sequence
     u = np.random.rand()
     r = currentDmg/newDmg # >1 means current is better, =1 means equivalent, <1 means new is better
     if (u < r):
-        return new # accept
+        return new, newDmg # accept
     else: 
-        return current # reject and keep current
+        return current, currentDmg # reject and keep current
