34a2fdd2dd08c6552f787391bde25ff24ead7215
Jordan <astrojord@gmail.com>
2021-09-12 17:27:07 -0400

    loop for MC steps + initial sequence

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 import numpy as np
 import sys
 from mcStep import *
-from Ability import * 
+from damagePlot import *
+from Ability import Ability
+from SelfParam import SelfParam
+from TargetParam import TargetParam
 
 # EXPECTED COMMAND LINE USAGE:
 # $ python main.py [args in an order that i will determine later]
 
 def main(args):
-    # woooo
     # unpack args
-    # instantiate player and target 
+    n, it = args[0:1]
+
+    # instantiate player and target, then calculate the hit chance and ability damage
     # instantiate abilities
     # start with n length sequence of all wrack/piercing shot/slice
-    # run MCMC for specified iterations
-    # store damage of each iteration
-    # plot damage over each iteration and return final sequence 
+    startSequence = []
+    if (player.weaponStyle == "magic"):
+        startSequence = [wrack] * n
+    elif (player.weaponStyle == "ranged"):
+        startSequence = [piercingShot] * n
+    elif (player.weaponStyle == "melee"):
+        startSequence = [_slice] * n
+
+    # run MCMC for specified iterations and store damage for each iteration
+    j = 0
+    nextSequence = startSequence
+    damageList = np.empty(it, dtype=int)
+    while (j < it):
+        nextSequence, damageList[j] = mcStep(n, nextSequence, player, target, abilities, hitChance, abilityDmg)
+        # how am I going to define abilities array in a nice pythonic way?
+        j += 1
+    
+    # plot damage over each iteration and return final sequence
+    damagePlot(n, it, damageList)
 
 if __name__ == '__main__':
     args = sys.argv
